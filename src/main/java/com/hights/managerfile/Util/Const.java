6402f94819ca214b3d415de015b48e1795b8ca04
yuyuan520 <809374044@qq.com>
2019-07-24 16:55:34 +0800

    添加文件转化功能

--- src/main/java/com/hights/managerfile/Util/Const.java
+++ src/main/java/com/hights/managerfile/Util/Const.java
@@ -1,6 +1,24 @@
 package com.hights.managerfile.Util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
 public class Const {
 
-    public final  static  String path ="E:/filepath";
+    public  static String getUploadFileName(){
+        SimpleDateFormat sf=new SimpleDateFormat("yyyyMMddHHmmssSSS");
+        Random random = new Random();
+        int ran = random.nextInt(999999);
+        String time=sf.format(new Date())+ran;
+        return time;
+    }
+
+    public static String pagepath(){
+
+        SimpleDateFormat simleDateFormat = new SimpleDateFormat( "yyyyMMdd" );
+        return simleDateFormat.format(new Date(  ));
+    }
+
+
 }
