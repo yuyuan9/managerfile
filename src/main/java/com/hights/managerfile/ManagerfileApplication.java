de6e23f2eed37fba889e1fbf527540223b82549b
yuyuan520 <809374044@qq.com>
2019-07-25 16:02:27 +0800

    打成war

--- src/main/java/com/hights/managerfile/ManagerfileApplication.java
+++ src/main/java/com/hights/managerfile/ManagerfileApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import javax.servlet.MultipartConfigElement;
@@ -15,7 +16,7 @@ import javax.servlet.MultipartConfigElement;
 @SpringBootApplication
 @ServletComponentScan
 
-public class ManagerfileApplication {
+public class ManagerfileApplication  extends SpringBootServletInitializer {
 
     @Value("${upload.filesize}")
     private String filesize;
